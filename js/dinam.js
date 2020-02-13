3ecda76ee0f6e27f4b399ccb3093c6795b5635b5
Bryan <bryher_msm@hotmail.com>
2020-02-13 16:28:35 -0500

    responvise2

--- js/dinam.js
+++ js/dinam.js
@@ -1,7 +1,8 @@
-const btnToggle = document.querySelector('.icon-menu');
+const btnToggle = document.querySelector('#menico');
 
 btnToggle.addEventListener('click', function () {
   console.log('clik')
   document.getElementById('menu').classList.toggle('active');
+  document.getElementById('menico').classList.toggle('active1');
   console.log(document.getElementById('menu'))
 });
