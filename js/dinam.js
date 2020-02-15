84efde1c3022b21b451a3dc262d2c39c5ce2f83e
Bryan <bryher_msm@hotmail.com>
2020-02-14 19:56:17 -0500

    menu

--- js/dinam.js
+++ js/dinam.js
@@ -6,3 +6,11 @@ btnToggle.addEventListener('click', function () {
   document.getElementById('menico').classList.toggle('active1');
   console.log(document.getElementById('menu'))
 });
+
+const socialToggle = document.querySelector('#menu-share');
+
+socialToggle.addEventListener('click', function () {
+  console.log('clik')
+  document.getElementById('social-toggle').classList.toggle('active3');
+  console.log(document.getElementById('social-toggle'))
+});
