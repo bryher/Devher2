ab7f3fe5233078a0d2aeba389c09f4a03871485c
Bryan <bryher_msm@hotmail.com>
2020-02-20 19:08:37 -0500

    low

--- js/dinam.js
+++ js/dinam.js
@@ -14,3 +14,31 @@ socialToggle.addEventListener('click', function () {
   document.getElementById('social-toggle').classList.toggle('active3');
   console.log(document.getElementById('social-toggle'))
 });
+
+const sunclic = document.querySelector('.sun');
+
+sunclic.addEventListener('click', function () {
+  console.log('clik')
+  document.getElementById('low').classList.toggle('brillo');
+  document.getElementById('low2').classList.toggle('brillo');
+  document.getElementById('low3').classList.toggle('brillo');
+  document.getElementById('low4').classList.toggle('brillo');
+  document.getElementById('low5').classList.toggle('brillo');
+  document.getElementById('low6').classList.toggle('brillo');
+  document.getElementById('low7').classList.toggle('brillo');
+  document.getElementById('low8').classList.toggle('brillo');
+  document.getElementById('low9').classList.toggle('brillo');
+  document.getElementById('low10').classList.toggle('brillo');
+  document.getElementById('low11').classList.toggle('brillo');
+  document.getElementById('low12').classList.toggle('brillo');
+  document.getElementById('low13').classList.toggle('brillo');
+  document.getElementById('low14').classList.toggle('brillo');
+  document.getElementById('low15').classList.toggle('brillo');
+  document.getElementById('low16').classList.toggle('brillo');
+  document.getElementById('low17').classList.toggle('brillo');
+  document.getElementById('low18').classList.toggle('brillo');
+  document.getElementById('low19').classList.toggle('brillo');
+  document.getElementById('low20').classList.toggle('brillo');
+  document.getElementById('low21').classList.toggle('brillo');
+  console.log(document.getElementById('low'))
+});
