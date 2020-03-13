ee597e07848cda97f2d2c59fde0d91fabbe33f82
bryan <bryher>
2020-03-12 19:09:04 -0500

    m

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
 function registrar() {
-  console.log('diste clic')
+  var name = document.getElementById('first_name').value;
+  var apellido = document.getElementById('last_name').value;
+  var email = document.getElementById('email').value;
+  var contrasena = document.getElementById('password').value;
+
+
+  firebase.auth().createUserWithEmailAndPassword(name, apellido, email, contrasena).cath(function (error) {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+  });
 }
