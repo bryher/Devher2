b1a8fea6704bfb103d8949405e5c04346e2e0b37
bryan <bryher>
2020-03-13 18:16:50 -0500

    login-avance

--- js/app.js
+++ js/app.js
@@ -1,12 +1,71 @@
+
+// clic botón enviar datos
 function registrar() {
-  var name = document.getElementById('first_name').value;
-  var apellido = document.getElementById('last_name').value;
-  var email = document.getElementById('email').value;
-  var contrasena = document.getElementById('password').value;
+ var email = document.getElementById('email').value;
+ var name = document.getElementById('first_name').value;
+ var apellido = document.getElementById('last_name').value;
+ var password = document.getElementById('password').value;
+
+ firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // [START_EXCLUDE]
+        if (errorCode == 'auth/weak-password') {
+          alert('The password is too weak.');
+        } else {
+          alert(errorMessage);
+        }
+        console.log(error);
+        // [END_EXCLUDE]
+      });
+}
+
+function ingreso() {
+  var emailI = document.getElementById('emailI').value;
+  var passwordI = document.getElementById('passwordI').value;
+
+  firebase.auth().signInWithEmailAndPassword(emailI, passwordI).catch(function(error) {
+          // Handle Errors here.
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          // [START_EXCLUDE]
+          if (errorCode === 'auth/wrong-password') {
+            alert('Wrong password.');
+          } else {
+            alert(errorMessage);
+          }
+          console.log(error);
+          document.getElementById('quickstart-sign-in').disabled = false;
+          // [END_EXCLUDE]
+        });
+}
 
 
-  firebase.auth().createUserWithEmailAndPassword(name, apellido, email, contrasena).cath(function (error) {
-    var errorCode = error.code;
-    var errorMessage = error.message;
-  });
+// Observador del usuario
+function observador() {
+  firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    console.log('activo');
+    activo();
+    // User is signed in.
+    var displayName = user.displayName;
+    var email = user.email;
+    var emailVerified = user.emailVerified;
+    var photoURL = user.photoURL;
+    var isAnonymous = user.isAnonymous;
+    var uid = user.uid;
+    var providerData = user.providerData;
+    // ...
+  } else {
+    // User is signed out.
+    // ...
+    console.log('no activo')
+  }
+});
+}
+observador();
+
+function activo() {
+  window.open("https://devher.space/", "_self")
 }
--- js/dinam.js
+++ js/dinam.js
@@ -22,3 +22,39 @@ sunclic.addEventListener('click', function () {
   document.getElementById('low').classList.toggle('brillo');
   console.log(document.getElementById('low'))
 });
+
+
+// verificador de usuario
+
+// Observador del usuario
+function observador() {
+  firebase.auth().onAuthStateChanged(function(user) {
+  if (user) {
+    console.log('activo')
+    // User is signed in.
+    var displayName = user.displayName;
+    var email = user.email;
+    var emailVerified = user.emailVerified;
+    var photoURL = user.photoURL;
+    var isAnonymous = user.isAnonymous;
+    var uid = user.uid;
+    var providerData = user.providerData;
+
+    // ...
+  } else {
+    // User is signed out.
+    // ...
+    abrirEnPestana(url)
+  }
+});
+}
+
+// funcion para que se habra nueva ventana al dar click en icono de inicio de secion
+
+function abrirEnPestana(url) {
+  var a = document.createElement("a");
+  a.href = url;
+  a.click();
+}
+
+var url="./login-sign/";
