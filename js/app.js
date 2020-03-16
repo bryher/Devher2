90dae4adc55fbf3f747813773a211d578c49fe92
bryan <bryher>
2020-03-16 13:51:09 -0500

    j

--- js/app.js
+++ js/app.js
@@ -29,28 +29,28 @@ function activo() {
 }
 // autenticacion con google
 
-var provider = new firebase.auth.GoogleAuthProvider();
+// var provider = new firebase.auth.GoogleAuthProvider();
+//
+// function registrarGoogle(){
+//
+//   firebase.auth().signInWithRedirect(provider).then(function(result) {
+//   // This gives you a Google Access Token. You can use it to access the Google API.
+//   var token = result.credential.accessToken;
+//   // The signed-in user info.
+//   var user = result.user;
+//   // ...
+// }).catch(function(error) {
+//   // Handle Errors here.
+//   var errorCode = error.code;
+//   var errorMessage = error.message;
+//   // The email of the user's account used.
+//   var email = error.email;
+//   // The firebase.auth.AuthCredential type that was used.
+//   var credential = error.credential;
+//   // ...
+// });
 
-function registrarGoogle(){
-
-  firebase.auth().signInWithRedirect(provider).then(function(result) {
-  // This gives you a Google Access Token. You can use it to access the Google API.
-  var token = result.credential.accessToken;
-  // The signed-in user info.
-  var user = result.user;
-  // ...
-}).catch(function(error) {
-  // Handle Errors here.
-  var errorCode = error.code;
-  var errorMessage = error.message;
-  // The email of the user's account used.
-  var email = error.email;
-  // The firebase.auth.AuthCredential type that was used.
-  var credential = error.credential;
-  // ...
-});
-
-}
+// }
  // autenticacion con facebook
 
   var provider = new firebase.auth.FacebookAuthProvider();
