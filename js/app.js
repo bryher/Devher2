1929f254b97aa94033fb4cb81da13348a9dd43d7
bryan <bryher>
2020-03-16 11:21:50 -0500

    facebook

--- js/app.js
+++ js/app.js
@@ -1,46 +1,4 @@
 
-// clic botón enviar datos
-function registrar() {
- var email = document.getElementById('email').value;
- var name = document.getElementById('first_name').value;
- var apellido = document.getElementById('last_name').value;
- var password = document.getElementById('password').value;
-
- firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // [START_EXCLUDE]
-        if (errorCode == 'auth/weak-password') {
-          alert('The password is too weak.');
-        } else {
-          alert(errorMessage);
-        }
-        console.log(error);
-        // [END_EXCLUDE]
-      });
-}
-
-function ingreso() {
-  var emailI = document.getElementById('emailI').value;
-  var passwordI = document.getElementById('passwordI').value;
-
-  firebase.auth().signInWithEmailAndPassword(emailI, passwordI).catch(function(error) {
-          // Handle Errors here.
-          var errorCode = error.code;
-          var errorMessage = error.message;
-          // [START_EXCLUDE]
-          if (errorCode === 'auth/wrong-password') {
-            alert('Wrong password.');
-          } else {
-            alert(errorMessage);
-          }
-          console.log(error);
-          document.getElementById('quickstart-sign-in').disabled = false;
-          // [END_EXCLUDE]
-        });
-}
-
 
 // Observador del usuario
 function observador() {
